0cf50ee58431fac928007f525dfb1b9c4d69ac1d
Debiprasad Das <debiprasaddaskdv@gmail.com>
2021-11-08 09:19:36 +0530

    Update and rename fluid_lavender.mk to nad_lavender.mk

--- nad_lavender.mk
+++ nad_lavender.mk
@@ -9,8 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common fluid stuff
-$(call inherit-product, vendor/fluid/config/common_full_phone.mk) 
+# Inherit some common nusantara stuff
+$(call inherit-product, vendor/nad/config/common_full_phone.mk) 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
@@ -20,7 +20,7 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := fluid_lavender
+PRODUCT_NAME := nad_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
