ea736c43f6b42bb280feb1806f83f0acd988750a
Debiprasad Das <debiprasaddaskdv@gmail.com>
2021-11-20 01:00:16 +0530

    Update and rename fluid_lavender.mk to arrow_lavender.mk

--- arrow_lavender.mk
+++ arrow_lavender.mk
@@ -9,8 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common nusantara stuff
-$(call inherit-product, vendor/fluid/config/common_full_phone.mk) 
+# Inherit some common arrow os stuff
+$(call inherit-product, vendor/arrow/config/common_full_phone.mk) 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
@@ -20,7 +20,7 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := fluid_lavender
+PRODUCT_NAME := arrow_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
@@ -35,7 +35,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT :="google/coral/coral:11/RQ3A.211001.001/7641976:user/release-keys"
 
 # Fluid stuff
-FLUID_BUILD_TYPE := UNOFFICIAL
-PRODUCT_PRODUCT_PROPERTIES += \
-ro.fluid.maintainer=DEBIPRASAD
-ro.fluid.cpu=SDM660
+#FLUID_BUILD_TYPE := UNOFFICIAL
+#PRODUCT_PRODUCT_PROPERTIES += \
+#ro.fluid.maintainer=DEBIPRASAD
+#ro.fluid.cpu=SDM660
